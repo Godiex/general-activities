4aebdded06c77b79e784b2af4a48edc4401d3365
diego andres villa cardona <spaydiego@hotmail.com>
2021-12-02 06:30:22 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -17,14 +17,11 @@ ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
           <BrowserRouter>
-              <App />
+
           </BrowserRouter>
       </Provider>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
